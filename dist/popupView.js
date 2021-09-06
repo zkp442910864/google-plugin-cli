64305fd718dd78505f62ebd9218d437acbc24ee4
zhoukaipeng <zhoukaipeng@irobotbox.local>
2021-09-06 14:51:04 +0800

    config🔧:

--- dist/popupView.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,a as o}from"./HelloWorld.js";import{d as a,o as l,c as e,a as p,F as r,b as t,e as u}from"./vendor.js";const i=t("img",{alt:"Vue logo",src:o},null,-1),m=t("div",null,"popupView",-1);u(a({setup:o=>(o,a)=>(l(),e(r,null,[i,p(s,{msg:"Hello Vue 3 + TypeScript + Vite"}),m],64))})).mount("#app");
--- dist/tagView.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,a as s}from"./HelloWorld.js";import{d as a,o as l,c as e,a as r,F as t,b as i,e as m}from"./vendor.js";const n=i("img",{alt:"Vue logo",src:s},null,-1),p=i("div",null,"tagView",-1);var u=a({setup:s=>(s,a)=>(l(),e(t,null,[n,r(o,{msg:"Hello Vue 3 + TypeScript + Vite"}),p],64))});console.log(123123),m(u).mount("#app");
